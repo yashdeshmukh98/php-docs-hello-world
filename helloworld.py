907aa87f8eb89c16524da8f99baedcdb6d8fe472
yashdeshmukh98 <yashdeshmukh0905@gmail.com>
2025-08-13 10:43:29 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,1 @@
-#!/usr/bin/env python
-
-"""Top-level script to invoke helloworld implementation."""
-
-import sys
-import helloworld.main
-
-if __name__ == '__main__':
-    sys.exit(helloworld.main.main())
+print("hello world")
